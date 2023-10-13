ae97e4354269434116c871b1903ed2f4f213122e
David Lima <antdavidlima@gmail.com>
2023-10-13 09:42:33 -0300

    configure nvim-treesitter

---
+++ lua/etc/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+local M = {
+ 'nvim-treesitter/nvim-treesitter',
+ build = ':TSUpdate',
+ event = 'BufEnter',
+ dependencies = {
+  {'windwp/nvim-ts-autotag'}
+ }
+}
+
+M.config = function()
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+      'css',
+      'html',
+      'javascript',
+      'typescript',
+      'tsx',
+      'lua',
+      'yuck',
+    },
+    highlight = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+    },
+  })
+end
+
+return M
