9d4696373bcff195c49ec98c0ff53827e2d79287
David Lima <antdavidlima@gmail.com>
2023-10-24 02:24:33 -0300

    add some visual and life quality options

--- lua/etc/options.lua
+++ lua/etc/options.lua
@@ -5,3 +5,7 @@ vim.opt.shiftwidth = 2
 vim.opt.cursorline = true
 vim.opt.numberwidth = 1
 vim.opt.signcolumn = "yes"
+vim.opt.backup = false
+vim.opt.showmode = false
+vim.opt.undofile = true
+vim.opt.laststatus = 0
