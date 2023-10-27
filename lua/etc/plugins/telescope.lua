f657072f3aba911a521d174fd7150ebdce6ad3f9
David Lima <antdavidlima@gmail.com>
2023-10-27 16:21:36 -0300

    add telescope shortcuts and change space to leader on lspconfig shortcuts

--- lua/etc/plugins/telescope.lua
+++ lua/etc/plugins/telescope.lua
@@ -18,6 +18,17 @@ local M = {
 
 M.config = function()
 	require('telescope').load_extension('projects')
+
+	vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
+	vim.keymap.set("n", "<leader>fi", ":Telescope current_buffer_fuzzy_find<CR>")
+	vim.keymap.set("n", "<leader>fd", ":Telescope diagnostics<CR>")
+	vim.keymap.set("n", "<leader>ff", ":Telescope fd<CR>")
+	vim.keymap.set("n", "<leader>fg", ":Telescope git_files<CR>")
+	vim.keymap.set("n", "<leader>ft", ":Telescope live_grep<CR>")
+	vim.keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>")
+	vim.keymap.set("n", "<leader>fq", ":Telescope planets<CR>")
+	vim.keymap.set("n", "<leader>fp", ":Telescope projects<CR>")
+	vim.keymap.set("n", "<leader>fs", ":Telescope spell_suggest<CR>")
 end
 
 return M
