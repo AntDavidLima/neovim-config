32a28e1918847bc0887fe95b4c1eb689c87a020c
David Lima <antdavidlima@gmail.com>
2023-10-28 01:18:36 -0300

    config toggleterm to open lazygit

--- lua/etc/plugins/toggleterm.lua
+++ lua/etc/plugins/toggleterm.lua
@@ -1,22 +1,28 @@
 local M = {
 	'akinsho/toggleterm.nvim',
-	keys = { [[<C-\>]] }
+	keys = { [[<C-\>]], [[<leader>g]] }
 }
 
 M.config = function ()
 	require('toggleterm').setup({
-		size =  function (term)
-			if term.direction ==  "horizontal" then
-				return 8
-			elseif term.direction  == "float" then
-				return 60
-			end
-		end,
+		size = 8,
 		open_mapping = [[<C-\>]],
-		start_in_insert =  false,
+		start_in_insert =  true,
 		direction  =  'horizontal',
 		close_on_exit = true,
 	})
+
+	local Terminal = require("toggleterm.terminal").Terminal
+	local lazygit = Terminal:new({
+		cmd = 'lazygit',
+		direction = 'float',
+	})
+
+	function Toggle_lazygit()
+		lazygit:toggle()
+	end
+
+	vim.keymap.set('n', '<leader>g', Toggle_lazygit)
 end
 
 return M
