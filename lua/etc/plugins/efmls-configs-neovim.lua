e2160b3dc39578ed5e98adbe101aa2e49ad3a921
David Lima <antdavidlima@gmail.com>
2023-11-06 22:53:43 -0300

    install and config efmls-configs

---
+++ lua/etc/plugins/efmls-configs-neovim.lua
@@ -0,0 +1,9 @@
+local M = {
+	'creativenull/efmls-configs-nvim',
+	version = 'v1.x.x',
+	dependencies = {
+		'neovim/nvim-lspconfig',
+	}
+}
+
+return M
--- lua/etc/plugins/mason.lua
+++ lua/etc/plugins/mason.lua
@@ -1,24 +1,40 @@
 local M = {
-	'williamboman/mason.nvim',
+	"williamboman/mason.nvim",
 	dependencies = {
-		'neovim/nvim-lspconfig',
-		'williamboman/mason-lspconfig.nvim',
-		'hrsh7th/cmp-nvim-lsp',
-	}
+		"neovim/nvim-lspconfig",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"creativenull/efmls-configs-nvim",
+	},
 }
 
 M.config = function()
 	require("mason").setup()
 	require("mason-lspconfig").setup({
-		ensure_installed = { 'lua_ls', 'tailwindcss' }
+		ensure_installed = { "lua_ls", "tailwindcss", "efm" },
 	})
-	require("mason-lspconfig").setup_handlers {
+	require("mason-lspconfig").setup_handlers({
 		function(server_name)
-			require("lspconfig")[server_name].setup {
-				capabilities = require('cmp_nvim_lsp').default_capabilities()
-			}
+			require("lspconfig")[server_name].setup({
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+			})
 		end,
-	}
+		["efm"] = function()
+			local languages = require("efmls-configs.defaults").languages()
+
+			require("lspconfig").efm.setup(vim.tbl_extend("force", {
+				filetypes = vim.tbl_keys(languages),
+				settings = {
+					rootMarkers = { ".git" },
+					languages = languages,
+				},
+				init_options = {
+					documentFormatting = true,
+					documentRangeFormatting = true,
+				},
+			}, {}))
+		end,
+	})
 
 	vim.keymap.set("n", "<Space>m", ":Mason<CR>")
 end
