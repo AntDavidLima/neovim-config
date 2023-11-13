3edc7d00086f37e18552b49d82b02ce30fe43e32
David Lima <antdavidlima@gmail.com>
2023-11-13 01:43:23 -0300

    configure format on save

--- lua/etc/plugins/none-ls.lua
+++ lua/etc/plugins/none-ls.lua
@@ -9,6 +9,8 @@ M.config = function()
 	local diagnostics = null_ls.builtins.diagnostics
 	local code_actions = null_ls.builtins.code_actions
 
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 	null_ls.setup({
 		sources = {
 			formatting.stylua,
@@ -16,6 +18,18 @@ M.config = function()
 			code_actions.eslint,
 			diagnostics.eslint,
 		},
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
+			end
+		end,
 	})
 
 	vim.keymap.set("n", "<leader>lF", ":NullLsInfo<CR>")
