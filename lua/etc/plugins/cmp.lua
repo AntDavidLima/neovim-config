318a2efad171458316609fac450ae7901c8cc518
David Lima <antdavidlima@gmail.com>
2023-11-14 16:53:52 -0300

    disable required fileds diagnostics on cmp.lua file

--- lua/etc/plugins/cmp.lua
+++ lua/etc/plugins/cmp.lua
@@ -1,66 +1,64 @@
 local M = {
-	'hrsh7th/nvim-cmp',
+	"hrsh7th/nvim-cmp",
 	dependencies = {
-		'L3MON4D3/LuaSnip',
-		'hrsh7th/cmp-nvim-lsp',
-		'onsails/lspkind-nvim',
-		'windwp/nvim-autopairs',
-		'saadparwaiz1/cmp_luasnip',
-	}
+		"L3MON4D3/LuaSnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"onsails/lspkind-nvim",
+		"windwp/nvim-autopairs",
+		"saadparwaiz1/cmp_luasnip",
+	},
 }
 
 M.config = function()
-	local cmp = require('cmp')
+	local cmp = require("cmp")
 	local luasnip = require("luasnip")
 
+	---@diagnostic disable-next-line: missing-fields
 	cmp.setup({
 		snippet = {
 			expand = function(args)
-				require('luasnip').lsp_expand(args.body)
-			end
+				require("luasnip").lsp_expand(args.body)
+			end,
 		},
 
 		mapping = cmp.mapping.preset.insert({
-			['<CR>'] = cmp.mapping.confirm({ select = true }),
-			['<Tab>'] = cmp.mapping(
-				function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expand_or_locally_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
 				end
-			),
-			['<C-j>'] = cmp.mapping.select_next_item(),
-			['<C-k>'] = cmp.mapping.select_prev_item(),
+			end),
+			["<C-j>"] = cmp.mapping.select_next_item(),
+			["<C-k>"] = cmp.mapping.select_prev_item(),
 		}),
 
 		sources = cmp.config.sources({
-			{ name = 'nvim_lsp' },
-			{ name = 'luasnip' },
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
 		}),
 
+		---@diagnostic disable-next-line: missing-fields
 		formatting = {
 			fields = {
-				"kind", "abbr", "menu"
+				"kind",
+				"abbr",
+				"menu",
 			},
 			format = require("lspkind").cmp_format({
-				menu = ({
+				menu = {
 					nvim_lsp = "[LSP]",
 					luasnip = "[LuaSnip]",
-				}),
-				mode = "kind"
+				},
+				mode = "kind",
 			}),
-		}
-
+		},
 	})
 
-	cmp.event:on(
-		'confirm_done',
-		require('nvim-autopairs.completion.cmp').on_confirm_done()
-	)
+	cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
 end
 
 return M
