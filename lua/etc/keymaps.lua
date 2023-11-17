b18e2bbefb8fb93608adc21d0605cfadf9f71221
David Lima <antdavidlima@gmail.com>
2023-11-17 15:05:44 -0300

    improve window, yank and paste management

--- lua/etc/keymaps.lua
+++ lua/etc/keymaps.lua
@@ -1,3 +1,14 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>z", vim.cmd.Lazy)
+
+vim.keymap.set("n", "<C-A-k>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-A-j>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-A-l>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-A-h>", ":vertical resize +2<CR>")
+
+vim.keymap.set("v", "p", '"_dP')
+vim.keymap.set("v", "<leader>p", "p")
+
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>p", '"+p')
