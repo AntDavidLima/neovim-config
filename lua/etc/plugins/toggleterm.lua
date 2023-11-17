cd4ddb2399513c9c7d0e5ce861e2c1f6db1d0590
David Lima <antdavidlima@gmail.com>
2023-11-17 15:47:20 -0300

    enable keymaps on terminal

--- lua/etc/plugins/toggleterm.lua
+++ lua/etc/plugins/toggleterm.lua
@@ -1,27 +1,29 @@
 local M = {
-	'akinsho/toggleterm.nvim',
+	"akinsho/toggleterm.nvim",
 }
 
-M.config = function ()
-	require('toggleterm').setup({
+M.config = function()
+	require("toggleterm").setup({
 		size = 8,
 		open_mapping = [[<C-\>]],
-		start_in_insert =  true,
-		direction  =  'horizontal',
+		start_in_insert = true,
+		direction = "horizontal",
 		close_on_exit = true,
+		insert_mappings = true,
+		terminal_mappings = true,
 	})
 
 	local Terminal = require("toggleterm.terminal").Terminal
 	local lazygit = Terminal:new({
-		cmd = 'lazygit',
-		direction = 'float',
+		cmd = "lazygit",
+		direction = "float",
 	})
 
 	function Toggle_lazygit()
 		lazygit:toggle()
 	end
 
-	vim.keymap.set('n', '<leader>g', Toggle_lazygit)
+	vim.keymap.set("n", "<leader>g", Toggle_lazygit)
 end
 
 return M
