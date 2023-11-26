0d9ad00c7869b6167e5c80ca9b64ea26f2c4c0cc
David Lima <antdavidlima@gmail.com>
2023-11-26 19:13:13 -0300

    move nvim tree related autocomands inside nvim tree file

--- lua/etc/plugins/nvim-tree.lua
+++ lua/etc/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 local M = {
-	'nvim-tree/nvim-tree.lua',
+	"nvim-tree/nvim-tree.lua",
 	dependencies = {
-		'nvim-tree/nvim-web-devicons',
+		"nvim-tree/nvim-web-devicons",
 	},
 }
 
@@ -11,7 +11,7 @@ M.config = function()
 		respect_buf_cwd = true,
 		update_focused_file = {
 			enable = true,
-			update_root = true
+			update_root = true,
 		},
 		hijack_cursor = true,
 		hijack_unnamed_buffer_when_opening = true,
@@ -20,16 +20,16 @@ M.config = function()
 			icons = {
 				glyphs = {
 					git = {
-						unstaged = '',
-						staged = '',
-						unmerged = '',
-						renamed = '',
-						untracked = '',
-						deleted = '',
-						ignored = ''
-					}
-				}
-			}
+						unstaged = "",
+						staged = "",
+						unmerged = "",
+						renamed = "",
+						untracked = "",
+						deleted = "",
+						ignored = "",
+					},
+				},
+			},
 		},
 		diagnostics = {
 			show_on_dirs = true,
@@ -68,11 +68,53 @@ M.config = function()
 				end
 			end
 
-			vim.keymap.set("n", "l", edit_or_open, opts('Edit or open'))
-		end
+			vim.keymap.set("n", "l", edit_or_open, opts("Edit or open"))
+		end,
 	})
 
 	vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+
+	vim.api.nvim_create_autocmd("QuitPre", {
+		callback = function()
+			local invalid_win = {}
+			local wins = vim.api.nvim_list_wins()
+			for _, w in ipairs(wins) do
+				local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+				if bufname:match("NvimTree_") ~= nil then
+					table.insert(invalid_win, w)
+				end
+			end
+			if #invalid_win == #wins - 1 then
+				-- Should quit, so we close all invalid windows.
+				for _, w in ipairs(invalid_win) do
+					vim.api.nvim_win_close(w, true)
+				end
+			end
+		end,
+	})
+
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
+		callback = function(data)
+			-- buffer is a directory
+			local directory = vim.fn.isdirectory(data.file) == 1
+
+			if not directory then
+				return
+			end
+
+			-- create a new, empty buffer
+			vim.cmd.enew()
+
+			-- wipe the directory buffer
+			vim.cmd.bw(data.buf)
+
+			-- change to the directory
+			vim.cmd.cd(data.file)
+
+			-- open the tree
+			require("nvim-tree.api").tree.open()
+		end,
+	})
 end
 
 return M
